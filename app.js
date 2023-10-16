463af97368f5b25e29ff24fc56d5af23eec07c7a
Suman Kumar Bisoi <80659900+suman19112001@users.noreply.github.com>
2023-10-16 13:53:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.get('/api/books/search/:query', async (req, res) => {
   }
 });
 
-// Start the server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
