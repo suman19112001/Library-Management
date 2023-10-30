03467f5cbbb4bdda972bbe239bb0c1a8ea51e134
Suman Kumar Bisoi <80659900+suman19112001@users.noreply.github.com>
2023-10-30 22:29:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/api/books', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
 app.get('/api/books/:genre', async (req, res) => {
   const genre = req.params.genre;
 
